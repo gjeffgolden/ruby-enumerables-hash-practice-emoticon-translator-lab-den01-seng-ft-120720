9a5e65ffc75550f307c0f377301bd5b8bb4966a8
Jeff Golden <gjgolden08@gmail.com>
2020-11-28 17:08:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,11 @@
 # require modules here
 
 require 'yaml'
+require 'pry'
 
 def load_library
-  YAML.load 
-  
-  
+  emoticon_hash = YAML.load(emoticons.rb) 
+  binding.pry 
 end
 
 def get_japanese_emoticon
