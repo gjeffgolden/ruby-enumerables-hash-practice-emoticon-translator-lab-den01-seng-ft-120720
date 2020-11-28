503e796a46b316729b75682be3b624f4f5de8488
Jeff Golden <gjgolden08@gmail.com>
2020-11-28 17:54:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,12 +11,8 @@ def load_library(file_path)
   emoticon_hash 
 end
 
-def get_japanese_emoticon(hash)
-  japanese_emoticon = " "
-  emoticon_hash.each do |k,v|
-    japanese_emoticon = emoticon_hash[k][v][1]
-  end
-  japanese_emoticon
+def get_japanese_emoticon()
+  
 end
 
 def get_english_meaning
