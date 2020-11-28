24d0065ac97ff3d0fb109624627dcc7aa567a916
Jeff Golden <gjgolden08@gmail.com>
2020-11-28 17:37:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,34 +4,9 @@ require 'yaml'
 require 'pry'
 
 def load_library(file_path)
-  emoticon_hash = {
-    :angel => 
-      {:english => " ", :japanese => " "},
-    :angry => 
-      {:english => " ", :japanese => " "},
-    :bored => 
-      {:english => " ", :japanese => " "},
-    :confused => 
-      {:english => " ", :japanese => " "},
-    :embarrassed => 
-      {:english => " ", :japanese => " "},
-    :fish => 
-      {:english => " ", :japanese => " "},
-    :glasses => 
-      {:english => " ", :japanese => " "},
-    :grinning => 
-      {:english => " ", :japanese => " "},
-    :happy => 
-      {:english => " ", :japanese => " "},
-    :kiss => 
-      {:english => " ", :japanese => " "},
-    :sad => 
-      {:english => " ", :japanese => " "}, 
-    :surprised => 
-      {:english => " ", :japanese => " "},
-    :wink => 
-      {:english => " ", :japanese => " "}
-  }
+  emoticon_hash = {}
+  
+   
   emoticon_hash 
   
 end
