985e2ebe8123a1abb09b417be669b4219baa29a9
Jeff Golden <gjgolden08@gmail.com>
2020-11-28 18:14:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,6 @@ def get_english_meaning(file_path, emoticon)
     if v[:japanese] == emoticon 
       return k 
     end 
-  puts "Sorry, that emoticon was not found"
   end
+  puts "Sorry, that emoticon was not found"
 end
