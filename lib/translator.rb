57dc89331c459850ba3b74edc419bdfed4d857db
Jeff Golden <gjgolden08@gmail.com>
2020-11-28 17:24:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,8 @@ require 'yaml'
 require 'pry'
 
 def load_library(file_path)
-  emoticon_hash{} = YAML.load("lib/emoticons.yml")
+  emoticon_hash = {}
+  emoticon_hash << YAML.load("lib/emoticons.yml")
   
   #binding.pry 
 end
